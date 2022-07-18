8c7ce40e77d6ce5378f911bc955bb268a56a947b
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-19 00:59:39 +0100

    push

--- personal_fun/list.py
+++ personal_fun/list.py
@@ -10,19 +10,19 @@ x.extend(["aye", 5])
 
 print(x)
 
-x.insert(4, "demola")
+x.insert(4, "demo")
 
 print(x)
 
 region = [["ogun", "osun", "lagos"], ["Imo", "Anambra", "Edo"], ["Kano", "kaduna", "kastina"]]
-# for a in region:
-#     for b in a:
-#         print(b)
+for a in region:
+    for b in a:
+        print(b)
 
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# for c in num:
-#     if c % 2 == 0:
-#         print(c)
+for c in num:
+    if c % 2 == 0:
+        print(c)
 
 ans = [c for c in num if c % 2 == 0]
 con = region + num
