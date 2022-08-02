9acb1b4290830eabe660dc257a9767405a8ec6b8
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-08-02 23:10:47 +0100

    push

--- personal_fun/cipher.py
+++ personal_fun/cipher.py
@@ -1,7 +1,6 @@
-def my_print(name):
-    print(f"hello {name}")
-    print(f"its me {name}")
-    print(f"make {name}")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-my_print("Amola")
+direction = input("Type 'encode' to encrypt and 'decode' to decrypt: ")
+text = input("Type your message: \n").lower()
+step = int(input("Type the shift number: \n"))
