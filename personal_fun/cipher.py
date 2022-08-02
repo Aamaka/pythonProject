5183858d13bece3f59631d132e3bd7708b825584
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-08-02 22:59:12 +0100

    push

---
+++ personal_fun/cipher.py
@@ -0,0 +1,7 @@
+def my_print(name):
+    print(f"hello {name}")
+    print(f"its me {name}")
+    print(f"make {name}")
+
+
+my_print("Amola")
--- personal_fun/hangman.py
+++ personal_fun/hangman.py
@@ -1,7 +1,9 @@
 import random
+import hang
 
 my_list = ["Lie", "Ada", "Obi", "James", "Hope", "Joy", "Tip", "lock", "unlock", "growth"]
 
+lives = 6
 chosen_word = random.choice(my_list)
 word_len = len(chosen_word)
 display = []
@@ -21,6 +23,14 @@ while not end_of_game:
             display[position] = word
     print(display)
 
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("you lose")
+
     if "_" not in display:
         end_of_game = True
         print("You win!")
+
+    print(hang.stages[lives])
