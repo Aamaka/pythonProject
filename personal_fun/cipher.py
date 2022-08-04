58134129297ea6f04325db94b4d7184c22aaaaa2
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-08-04 16:49:51 +0100

    push

--- personal_fun/cipher.py
+++ personal_fun/cipher.py
@@ -1,6 +1,27 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt and 'decode' to decrypt: ")
 text = input("Type your message: \n").lower()
 step = int(input("Type the shift number: \n"))
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"the {cipher_direction}d text is {end_text}")
+
+
+caesar(start_text=text, shift_amount=step, cipher_direction=direction)
